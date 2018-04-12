f779671749b9d4adb3d946d2f83d3d41f5f327ba
llr392425595 <392425595@qq.com>
2018-04-12 14:50:03 +0800

    补充practice3的sql语句

--- practice3.sql
+++ practice3.sql
@@ -1,4 +1,16 @@
 #取得平均薪水最高的公司
 #输出公司名称和平均薪水：companyName avgSalary
 
+# 找出所有公司及平均薪资
+select companyName, avg(salary) as avgSalary
+from Company left join Employee
+on Company.id = Employee.companyId
+group by companyId;
+
+# 找出最多
+select companyName, max(avgSalary) as avgSalary from
+(select companyName, avg(salary) as avgSalary
+from Company left join Employee
+on Company.id = Employee.companyId
+group by companyId) as t;
 
